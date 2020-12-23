379530d721835c754f21a694174bc7987dbcdf94
whynaa <whyna@smktelkom-mlg.sch.id>
2020-12-23 15:34:43 +0700

    php

---
+++ php1/while.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    // Deklarasikan variable $i 
+    $i = 2;
+    
+    // Ketik loop while dibawah
+    while($i <= 100){
+      echo $i.'<br>';
+      $i+=2;
+    } 
+    
+  ?>
+
+</body>
+</html>
+
